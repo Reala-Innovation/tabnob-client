06034837e9467d25482d460e0825d20f46548cb8
austinesamuel <austinesamuel914@gmail.com>
2025-07-12 16:28:30 +0100

    add title

--- src/components/presenter.tsx
+++ src/components/presenter.tsx
@@ -1,6 +1,8 @@
 import type React from "react"
+import { useInnerWidth } from "../hooks/useInnerWidth";
 
 const Presenter:React.FC = () => {
+  const width=useInnerWidth();
   return (
     <div style={{padding:16}}>
 <br/>
@@ -8,6 +10,11 @@ const Presenter:React.FC = () => {
 <br/>
 <br/>
 <br/>
+{width && <>
+<br/>
+<br/>
+<br/>
+</>}
       <h1 style={{fontSize:40}} className='headingFont'>Send Bitcoin & Receive Mobile Money instantly</h1>
 
     </div>
