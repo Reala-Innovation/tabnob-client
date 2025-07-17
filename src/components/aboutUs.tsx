68a80ac31030397970cc9446fd101842c9eb9ba4
mubarak23 <mubarakaminu340@gmail.com>
2025-07-17 12:57:18 +0100

    chore:fix content

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -9,10 +9,8 @@ const AboutUs = () => {
       <div className="about-text">
         <h3 className='headingFont-h3'>About Us</h3>
         <p>
-          At tapnob, we simplify spending Bitcoin across Africa. As part of BitcoinDua, we bridge the gap
-          between Bitcoin and Mobile Money (MoMo), letting you convert and use Bitcoin instantly. No complicated
-          exchanges, no long wait times — just fast, secure, and affordable payments for your everyday needs.
-          Our mission is to make Bitcoin adoption easy and accessible for everyone, one seamless transaction at a time.
+          At Tapnob, we simplify spending Bitcoin across Africa. We bridge the gap between Bitcoin and instant bank transfers, letting you convert and use Bitcoin effortlessly. No complicated exchanges, no long wait times — just fast, secure, and affordable payments for your everyday needs.
+          Our mission is to make Bitcoin adoption easy and accessible for everyone, one seamless bank transaction at a time
         </p>
       </div>
     </div>
