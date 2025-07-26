8b19d45e3bc3d79359dfde9ab77863ca282f18c8
Solomon Emmanuel <soulex04292003@gmail.com>
2025-07-26 15:46:52 +0100

    chore:beta banner

--- src/components/presenter.tsx
+++ src/components/presenter.tsx
@@ -18,6 +18,28 @@ const Presenter:React.FC = () => {
 <br/>
 <br/>
 </>}
+<div style={{
+  width:'100%',
+  display:'flex',
+  justifyContent:'center',
+  alignItems:"center"
+}}>
+<p style={{
+  backgroundColor: 'orange',
+  textAlign: 'center',
+  padding: '8px 16px',
+  margin: '16px auto',
+  display: 'inline-block',
+  borderRadius: '8px',
+  fontWeight: 'bold',
+  fontSize: '14px',
+  color: '#333'
+  
+}}>
+
+  Currently in beta
+</p>
+</div>
       <h1 data-aos={"fade-up"} style={{fontSize:40}} className='headingFont'>
         Spend Bitcoin. Keep Your Bitcoin.
 
